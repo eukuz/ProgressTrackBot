47b2eb1f6520b9112519995a793bcbe68205ab5e
eukuz <evgn.worker@gmail.com>
2021-09-15 11:45:13 +0300

    'dynamic' interface implemented

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
+from aiogram.utils.exceptions import MessageToDeleteNotFound
 from bson import ObjectId
 
 import keyboards
@@ -42,6 +43,16 @@ def format_id(process_id):
     return {'_id': ObjectId(process_id)}
 
 
+async def deleteMessages(state, msg, bot):
+    data = await state.get_data()
+    if 'delete_list_from' and 'delete_to' in data:
+        for j in range(data['delete_list_from'],data['delete_to']):
+            try:
+                await bot.delete_message(msg.chat.id, j)
+            except MessageToDeleteNotFound:
+                continue
+
+
 def main():
     client = pymongo.MongoClient(CONNECTION)
     db = client[DB]
@@ -56,8 +67,10 @@ def main():
         await States.default.set()
 
     @dp.message_handler(lambda msg: msg.text == strings.GET_PROGRESSES, state=['*'])
-    async def get_progresses(msg: types.Message):
+    async def get_progresses(msg: types.Message, state: FSMContext):
+        await deleteMessages(state, msg, bot)
         i = 1
+        await state.update_data(delete_list_from=msg.message_id)
         progresses = col.find({"user_id": msg.from_user.id}).sort("priority", -1)
         if progresses is not None:
             for progress in progresses:
@@ -65,6 +78,7 @@ def main():
                 await bot.send_message(msg.from_user.id, progress_format(progress),
                                        reply_markup=make_progress_inline_kb(callback))
                 i += 1
+        await state.update_data(delete_to=msg.message_id+i)
 
     @dp.callback_query_handler(state=States.waiting)
     async def proceed_deletion(call: types.CallbackQuery):
@@ -111,18 +125,22 @@ def main():
             await States.waiting.set()
 
     @dp.message_handler(lambda msg: msg.text == strings.CREATE_PROGRESS, state=['*'])
-    async def create_progress(msg: types.Message):
+    async def create_progress(msg: types.Message, state: FSMContext):
+        await deleteMessages(state, msg, bot)
+        await state.update_data(delete_from=msg.message_id)
         await msg.reply(strings.INPUT_NAME)
         await States.name.set()
 
     @dp.message_handler(state=States.name, content_types=['text'])
     async def name_chosen(msg: types.Message, state: FSMContext):
+        await state.update_data(delete_to=msg.message_id+2)
         await state.update_data(name=msg.text)
         await msg.answer(strings.INPUT_NUMBER_OF_ELEMENTS)
         await States.next()
 
     @dp.message_handler(state=States.n_full, content_types=['text'])
     async def n_chosen(msg: types.Message, state: FSMContext):
+        await state.update_data(delete_to=msg.message_id+2)
         try:
             n = int(msg.text)
             if n < 0:
@@ -138,6 +156,7 @@ def main():
 
     @dp.message_handler(state=States.deadline, content_types=['text'])
     async def deadline_chosen(msg: types.Message, state: FSMContext):
+        await state.update_data(delete_to=msg.message_id+2)
         try:
             d = datetime.strptime(msg.text, strings.DATE_FORMAT)
         except ValueError:
@@ -150,6 +169,7 @@ def main():
 
     @dp.message_handler(state=States.priority, content_types=['text'])
     async def priority_chosen(msg: types.Message, state: FSMContext):
+        await state.update_data(delete_to=msg.message_id+2)
         try:
             p = int(msg.text)
             if p < 0 or p > 100:
@@ -161,10 +181,17 @@ def main():
 
         await state.update_data(priority=p)
         data = await state.get_data()
-        data['user_id'] = msg.from_user.id
-        data['n_completed'] = 0
-        await msg.answer(strings.CREATED.format(data['name'], data['n_full'], data['deadline'], data['priority']))
-        col.insert_one(data)
+        d = data.copy()
+        d['user_id'] = msg.from_user.id
+        d['n_completed'] = 0
+        delete_id = d['delete_from']
+        d.pop('delete_from')
+        d.pop('delete_to')
+        for i in range(delete_id, msg.message_id + 1):
+            await bot.delete_message(msg.chat.id, i)
+
+        await msg.answer(strings.CREATED.format(d['name'], d['n_full'], d['deadline'], d['priority']))
+        col.insert_one(d)
         await States.default.set()
 
     if __name__ == '__main__':
